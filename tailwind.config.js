b6a6384d99c4b3ffa612afe3dd0b2a8aa7264e54
aungaung3457 <163144801+aungaung3457@users.noreply.github.com>
2025-10-17 00:47:44 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'beau-rivage': ['Beau Rivage', 'cursive'],
+        'jost': ['Jost', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1D3408',
+        'primary-dark': '#7C3AED',
+        secondary: '#FB7D5B',
+        accent: '#10B981',
+        tertiary: '#FCC43E',
+        quatinery: '#4D7E20',
+        danger: '#FF4550',
+      },
+    },
   },
   plugins: [],
 }
-
