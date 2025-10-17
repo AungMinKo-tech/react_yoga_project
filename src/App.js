7d1c27de7ab1325bd0d15c4c20b046d1da2d586a
May Mon Hnin <maymonhnin1000@gmail.com>
2025-10-17 09:38:43 +0630

    feat: merge with dev/amz(font, Color, and Home, About pages)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';   
 
-import Home from './components/Home.jsx';        
-import AboutUs from './components/AboutUs.jsx';  
+import Home from './pages/Home.jsx';        
+import AboutUs from './pages/AboutUs.jsx';  
 import Navbar from './components/Navbar.jsx';
 // import Footer from '';
 // import Program from '';
